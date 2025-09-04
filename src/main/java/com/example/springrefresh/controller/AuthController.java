7396f9004669da7910692c50b6a99c20c918f803
notlikelion <notlikelion@gmail.com>
2025-09-04 14:11:52 +0900

    푸시

--- src/main/java/com/example/springrefresh/controller/AuthController.java
+++ src/main/java/com/example/springrefresh/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.example.springrefresh.controller;
 
 import com.example.springrefresh.RefreshTokenRepository;
 import com.example.springrefresh.util.JwtUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -45,9 +47,24 @@ public class AuthController {
         refreshTokenRepository.save(username, refreshToken);
 
         // 쿠키에 담아주는 과정
-        // TODO : ???
+        response.addCookie(createCookie("refreshToken", refreshToken));
         // JSON 바디에 AccessToken Return
         // List.of(e1, e2, e3...) Map.of(k1, v1, k2, v2, ...)
         return ResponseEntity.ok(Map.of("accessToken", accessToken));
+    }
+
+    @Value("${jwt.refresh-token-expiration}")
+    private Long refreshExpirationMs; // 필드 주입
+
+    // HttpOnly
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+//        cookie.setMaxAge(60 * 60 * 24 * 7); // refresh token
+//        cookie.setMaxAge(60 * 5); // refresh token
+        cookie.setMaxAge((int) (refreshExpirationMs / 1000)); // refresh token
+        cookie.setHttpOnly(true); // 보안 설정
+        cookie.setPath("/");
+//        cookie.setSecure(true); // HTTPS -> 뒤에 배포까지 전제한 실습에서는 적용.
+        return cookie;
     }
 }
