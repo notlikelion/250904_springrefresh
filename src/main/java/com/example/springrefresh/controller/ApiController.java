e507dd77d342d31636404b665f9fd02d7992d101
notlikelion <notlikelion@gmail.com>
2025-09-04 13:43:56 +0900

    푸시

---
+++ src/main/java/com/example/springrefresh/controller/ApiController.java
@@ -0,0 +1,5 @@
+package com.example.springrefresh.controller;
+
+public class ApiController {
+
+}
---
+++ src/main/java/com/example/springrefresh/controller/AuthController.java
@@ -0,0 +1,53 @@
+package com.example.springrefresh.controller;
+
+import com.example.springrefresh.RefreshTokenRepository;
+import com.example.springrefresh.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtUtil jwtUtil;
+
+    public record LoginRequest(String username, String password) {}
+
+    @PostMapping("/login")
+    // "{accessToken: ~}"
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        // 사용자 인증
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+        // 인증 성공시 UserDetails
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().iterator().next().getAuthority()
+                .replace("ROLE_", "");
+        // ROLE_USER -> JWT
+
+        // 토큰 생성
+        String accessToken = jwtUtil.createToken(username, role, "access");
+        String refreshToken = jwtUtil.createToken(username, role, "refresh");
+
+        // 리프레시 토큰 -> 서버 관리
+        refreshTokenRepository.save(username, refreshToken);
+
+        // 쿠키에 담아주는 과정
+        // TODO : ???
+        // JSON 바디에 AccessToken Return
+        // List.of(e1, e2, e3...) Map.of(k1, v1, k2, v2, ...)
+        return ResponseEntity.ok(Map.of("accessToken", accessToken));
+    }
+}
