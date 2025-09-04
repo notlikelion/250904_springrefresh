e1439c95beb9df7842f61483154985e0be820578
notlikelion <notlikelion@gmail.com>
2025-09-04 13:13:37 +0900

    푸시

--- src/main/java/com/example/springrefresh/config/SecurityConfig.java
+++ src/main/java/com/example/springrefresh/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.springrefresh.config;
 
+import com.example.springrefresh.filter.JwtFilter;
 import com.example.springrefresh.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -38,10 +48,36 @@ public class SecurityConfig {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         // 경로별 보안설정
-        // ?
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/reissue").permitAll() // 모두가 요청할 수 있게
+                .requestMatchers("/api/**").hasRole("USER") // 특정한 권한만
+                .anyRequest().authenticated() // 로그인만하면...
+        );
+
         // JWT 필터 추가
-        // ?
+        http.addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class); // UPAF
         return http.build();
+    }
+
+    // 1. PasswordEncoder -> 로그인 시도나 가입 할 때 -> 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    // 2. UserDetails <- ???
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user") // id : user
+                .password(passwordEncoder().encode("1234")) // pw : 1234
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    // 3. Authentication Manager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
